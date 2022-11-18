b02ef9ec2649c8229e5b50449c804b6e0cda8f46
flareis <flavia.reis@dasa.com.br>
2022-11-18 17:31:33 -0300

    feature: fundamentos part III

--- 2_FUNDAMENTOS/9_lendo_input/index.js
+++ 2_FUNDAMENTOS/9_lendo_input/index.js
@@ -1,10 +1,10 @@
-const realine = require("readline").createInterface({
+const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 })
 readline.question("Qual a sua linguagem preferida?", (language) => {
   if(language === "Python"){  
-    console.log("è uma ótima linguagem de programação.")
+    console.log("É uma ótima linguagem de programação.")
   }else{
   console.log(`A minha linguagem preferida é: ${language}`)
   }
